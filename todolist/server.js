1c3aae956141f5bea35205b5038cccda6e82d4d9
racheljdietz <97128696+racheljdietz@users.noreply.github.com>
2022-01-12 15:44:28 -0500

    Update server.js

--- todolist/server.js
+++ todolist/server.js
@@ -1,43 +1,66 @@
+require('dotenv').config();
 const express = require("express");
-const app = express();
-
-// route to the root
-app.get('/', (req, res) => {
-    db.select('*').from('task').then(data => {
-        res.render('index', { todolist: data });
-    }).catch(err => res.status(400).json(err));
-});
-
-// server port connection
-app.listen(8080, ()=> console.log('app is running on port 8080'));
-
-// set up the express to use template engine
-app.set('view engine', 'ejs');
-
-const dotenv = require('dotenv');
-dotenv.config();
-
 const knex = require('knex');
+
 const db = knex({
     client: 'pg', // postgres
     connection: {
         host: process.env.HOST,
+        port: process.env.PORT,
         user: process.env.USER,
-        password: process.env.PASSWORD
+        password: process.env.PASSWORD,
+        database: process.env.NAME
     }
 });
 
-const bodyParser = require("body-parser");
-app.user(bodyParser.urlencoded({ extended: true }));
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public'));
+
+// res.render
+app.get('/', (req, res) => {
+    db.select("*").from("list").then( data => {
+        res.render('index', {todos: data});
+    }).catch(err => res.status(400).json(err));
+});
 
 // create new task
 app.post("/addTask", (req, res) => {
     const { textTodo } = req.body;
-    
-    db("task").insert({ task: textTodo }).returning("*").then(_=> {
-       res.redirect("/");
+    db("list").insert({task: textTodo}).returning("*").then(list => {
+        res.redirect("/");
     }).catch(err => {
-        res.status(400).json({ message: "unable to create a new task"
-       });
-    });
+        res.status(400).json({ message: "unable to create a new task" });
+      });
 });
+
+// mark task done
+app.post("/", (req, res) => {
+    const {name, id} = req.body;
+    if(name === "todo checked"){
+        db("list")
+        .where("id", "=", id)
+        .update("status", 1)
+        .then(task => {res.json(task[0])});
+    } else {
+        db("list")
+        .where("id", "=", id)
+        .update("status", 0)
+        .then(task => {res.json(task[0])});
+    }
+});
+
+// delete task
+app.delete("/", (req, res) => {
+    const {id} = req.body;
+    db("list")
+        .where("id", "=", id)
+        .del()
+        .then(task => {res.json(task[0])});
+});
+
+
+app.listen(8080, () => console.log("app is running on port 8080"));
